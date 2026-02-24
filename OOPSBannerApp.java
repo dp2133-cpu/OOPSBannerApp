587a38c54c1b7dd1f3e7cf6da0d85890a7985b83
P.Dhaaruni <dp2133@srmist.edu.in>
2026-02-24 13:27:00 +0530

    UC2: Render OOPS Banner and add gitignore

---
+++ OOPSBannerApp.java
@@ -0,0 +1,24 @@
+
+/**
+ * OOPSBannerApp
+ *
+ * UC2: Render OOPS as Banner using Print Statements
+ *
+ * @author HP
+ * @version 2.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        // Each letter is 7 lines height, width 9 characters
+        System.out.println("  *****   " + "  *****   " + " *******  " + " *******  ");
+        System.out.println(" *     *  " + " *     *  " + " *      * " + " *        ");
+        System.out.println(" *     *  " + " *     *  " + " *      * " + " *        ");
+        System.out.println(" *     *  " + " *     *  " + " *******  " + " *******  ");
+        System.out.println(" *     *  " + " *     *  " + " *        " + "        * ");
+        System.out.println(" *     *  " + " *     *  " + " *        " + "        * ");
+        System.out.println("  *****   " + "  *****   " + " *        " + " *******  ");
+
+    }
+}
