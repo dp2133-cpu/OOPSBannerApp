4d0c7f8aff00fb404fe39501eadd9bdb858ef763
P.Dhaaruni <dp2133@srmist.edu.in>
2026-03-01 22:51:37 +0530

    UC3: Refactored OOPS banner using String.join() for memory efficiency

---
+++ OOPSBannerAppB2P13/OOPSBannerApp.java.java
@@ -0,0 +1,23 @@
+public class Helloworld{
+	public static void main(String[] args){
+		System.out.println("OOPS");
+	}
+/**
+ * OOPSBannerApp
+ * 
+ * UC1: Print OOPS to Console
+ * This program prints "OOPS" to the console.
+ * 
+ * @author Dhaaruni
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the application
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
